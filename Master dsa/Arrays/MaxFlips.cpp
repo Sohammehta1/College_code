d34690f2608601c246cc802a4aac50b434261c41
= <=>
2024-01-27 17:14:23 +0530

    c

--- Master dsa/Arrays/MaxFlips.cpp
+++ Master dsa/Arrays/MaxFlips.cpp
@@ -2,32 +2,37 @@
 #include<vector>
 
 void max_con_flips(int arr[], int n){
-    
-    int i =0;
-    std :: vector<std :: pair<int,int>> max0,max1; // for keeping track of groups of ones and zeroes
-    while(i<n){
-        if(arr[i]==1)
-        {
-            int e = i;
-            while (arr[i]==1)
-                i++;
+
+    // NAIVE SOLUTIION : TC : O(2N); SC : O(N)
+    // int i =0;
+    // std :: vector<std :: pair<int,int>> max0,max1; // for keeping track of groups of ones and zeroes
+    // while(i<n){
+    //     if(arr[i]==1)
+    //     {
+    //         int e = i;
+    //         while (arr[i]==1)
+    //             i++;
             
-            max1.push_back({e,i-1});
-        }
-        else{
-            int e = i;
-            while (arr[i]==0)
-                i++;
+    //         max1.push_back({e,i-1});
+    //     }
+    //     else{
+    //         int e = i;
+    //         while (arr[i]==0)
+    //             i++;
             
-            max0.push_back({e,i-1});
-        }
-    }
-    if(max1.size()>max0.size())
-        for(std :: pair p :  max0)
-            std :: cout << p.first << " to " << p.second << std :: endl; 
-    else
-        for(std :: pair p :  max1)
-            std :: cout << p.first << " to " << p.second << std :: endl; 
+    //         max0.push_back({e,i-1});
+    //     }
+    // }
+    // if(max1.size()>max0.size())
+    //     for(std :: pair p :  max0)
+    //         std :: cout << p.first << " to " << p.second << std :: endl; 
+    // else
+    //     for(std :: pair p :  max1)
+    //         std :: cout << p.first << " to " << p.second << std :: endl; 
+
+    // A MORE TIME EFFICIENT SOLUTION
+
+    //  find out the minimum group and then traverse array again to print the indices.
 }
 
 int main(){
